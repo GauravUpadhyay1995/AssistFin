f9d27f9d12f799a9690ab89fd146cffd47bf1c93
GAURAV UPADHYAY <142603807+GauravUpadhyay1995@users.noreply.github.com>
2024-06-27 17:44:05 +0530

    databse changes

--- config/db.js
+++ config/db.js
@@ -1,40 +1,14 @@
-// import mysql from 'mysql';
-// import dotenv from 'dotenv';
-// dotenv.config();
-// const conn = mysql.createConnection({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_DATABSE,
-//     connectTimeout: 90000,
-
-// });
-
-
-// export default conn;
 import mysql from 'mysql';
 import dotenv from 'dotenv';
-import util from 'util';
-
 dotenv.config();
-
 const conn = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABSE,
     connectTimeout: 90000,
+
 });
 
-conn.connect((err) => {
-    if (err) {
-        console.error('Database connection failed:', err.stack);
-        return;
-    }
-    console.log('Connected to database.');
-});
 
-// Promisify the query method
-const query = util.promisify(conn.query).bind(conn);
-
-export default { query };
+export default conn;
